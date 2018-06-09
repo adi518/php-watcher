8e3b35fd9b2e38808a45d8f79eac85dcd0c5dd08
Adi Sahar <adisahar1@hotmail.com>
2018-06-09 03:50:52 +0300

    Update boilerplate

--- php/deploy.php
+++ php/deploy.php
@@ -2,8 +2,8 @@
   // https://stackoverflow.com/questions/4238433/php-how-to-create-a-newline-character
   // https://softwareengineering.stackexchange.com/questions/163004/what-is-the-opposite-of-initialize-or-init
   $sleep = 2;
-  echo('[deploy] => initialized' . PHP_EOL);
-  echo("[deploy] => async operations... (sleep $sleep" . 's)' . PHP_EOL);
+  echo('[deploy] initialized' . PHP_EOL);
+  echo("[deploy] async operations... (sleep $sleep" . 's)' . PHP_EOL);
   sleep($sleep);
-  echo ('[deploy] => finalized' . PHP_EOL);
+  echo ('[deploy] finalized' . PHP_EOL);
 ?>
