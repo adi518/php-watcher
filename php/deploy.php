238094037aa3de17342f06b7e1263ab8171f9744
Adi Sahar <adisahar1@hotmail.com>
2018-06-09 03:46:43 +0300

    Improve boilerplate

--- php/deploy.php
+++ php/deploy.php
@@ -1,9 +1,8 @@
 <?
   // https://stackoverflow.com/questions/4238433/php-how-to-create-a-newline-character
   $sleep = 2;
-  echo('Deploy => Start' . PHP_EOL);
-  echo("Deploy => Sleep $sleep" . 's' . PHP_EOL);
+  echo('[deploy] => initialized' . PHP_EOL);
+  echo("[deploy] => async operations... (sleep $sleep" . 's)' . PHP_EOL);
   sleep($sleep);
-  echo('Deploy => Wake up' . PHP_EOL);
-  echo ('Deploy => Finish' . PHP_EOL);
+  echo ('[deploy] => finalized' . PHP_EOL);
 ?>
---
+++ php/src/bar/bar.php
@@ -0,0 +1,6 @@
+<?
+  // Make some changes here, hit save and
+  // the watcher will execute the handler,
+  // in our case `deploy.php`.
+  echo('hello world!');
+?>
